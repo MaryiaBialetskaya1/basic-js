536f80e175e1e7120945008f771297d07fb1f002
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-04-28 17:31:44 -0400

    feat: implement task encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,7 +11,11 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function encodeLine(str) {
-  throw new NotImplementedError("Not implemented");
+  return str.replace(/([a-z])\1*/g, (it) =>
+    it.length > 1 ? it.length + it[0] : it[0]
+  );
+
+  //throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
 }
 
