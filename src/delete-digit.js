50c77a897c5e1a2ec936c9603d348b062124fe33
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-04-28 12:09:49 -0400

    feat: implement task get-email-domains, delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  let num = String(n).split("").map(Number);
+  return Math.max(
+    ...num.map((element, i) => {
+      let newNum = num.slice();
+      newNum.splice(i, 1);
+      return Number(newNum.join(""));
+    })
+  );
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,33 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  arr.flatMap((a, b, c) => {
+    if (c[b + 1] === "--discard-prev") return [];
+    if (c[b - 1] === "--double-next") return [a, a];
+    if (c[b + 1] === "--double-prev") return [a, a];
+
+    if (a.toString().startsWith("--d")) return [];
+
+    return a;
+  });
+
+  // throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
 }
 
 module.exports = {
-  transform
+  transform,
 };
