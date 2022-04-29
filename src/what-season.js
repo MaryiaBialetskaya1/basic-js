abe97333a03bed1d1abc8fcf930545ee337abf3b
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-04-29 10:56:50 -0400

    feat: partially implement what season task

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,31 @@ const { NotImplementedError } = require("../extensions/index.js");
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-
 function getSeason(date) {
   //throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
+
+  if (
+    !(date instanceof Date) ||
+    Object.getOwnPropertySymbols(date).length !== 0
+  ) {
+    throw new Error("Invalid date!");
+  }
+  let monthSeasons = [
+    "winter",
+    "winter",
+    "spring",
+    "spring",
+    "spring",
+    "summer",
+    "summer",
+    "summer",
+    "fall",
+    "fall",
+    "fall",
+    "winter",
+  ];
+  return monthSeasons[date.getMonth()];
 }
 
 module.exports = {
