0bf538998e32c4d372dc721d5b5190b30886fca2
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-04-29 11:51:37 -0400

    feat: try to implement task make chain(fail)

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,25 +6,34 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError("Not implemented");
+    //throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/*value*/) {
-    throw new NotImplementedError("Not implemented");
+  addLink(value) {
+    //throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
+    this.chain.push("" + value);
+    return this;
   },
   removeLink(/*position*/) {
     throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError("Not implemented");
+    //throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError("Not implemented");
+    //throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
+    this.chain = [];
+    let newChain = `( ${this.chain.join(" )~~( ")} )`;
+    return newChain;
   },
 };
 
